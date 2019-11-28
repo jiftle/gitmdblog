cfe3501ebfb1777feabcab32c36933de27129d40
lixu <lixu@newcapec.com>
2019-11-28 14:29:10 +0800

    博客文章目录通过配置文件配置

--- config/config.go
+++ config/config.go
@@ -1,5 +1,10 @@
 package config
 
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
 // ---------------- 定义变量 ----------------
 var (
 	siteName     = "HappeLife"
@@ -10,9 +15,36 @@ var (
 
 // 获取博客文章目录
 func GetBlogPostsDir() string {
+	readConfig()
 	return blogPostsDir
 }
 
 func GetSiteName() string {
 	return siteName
+}
+
+func readConfig() {
+
+	// 读取文件
+	//-------------- properties 文件 ----------------
+	config := viper.New()
+	config.AddConfigPath("./conf")
+	config.SetConfigType("yaml")
+	config.SetConfigName("gitmdblog")
+	if err := config.ReadInConfig(); err != nil {
+		fmt.Println("[x] 配置文件不存在，创建默认配置文件")
+	}
+
+	//---------- 写入配置文件 -------------
+	bakconf := fmt.Sprintf("./conf/gitmdblog.yaml")
+	if err := config.WriteConfigAs(bakconf); err != nil {
+		fmt.Println("[x] 创建默认配置文件失败")
+		panic(err)
+		config.Set("postsdir", "posts")
+		config.WriteConfig()
+	}
+	blogPostsDir = config.GetString("postsdir")
+	fmt.Println("[v] 读取配置文件成功")
+	//	msg := fmt.Sprintf("posts dir: %s\n", blogPostsDir)
+	//	fmt.Print(msg)
 }
