801ac24fcd6c81834c4c2d8393df03aba11d0983
lixu <lixu@newcapec.com>
2019-11-28 12:09:24 +0800

    w

--- config/config.go
+++ config/config.go
@@ -2,10 +2,10 @@ package config
 
 // ---------------- 定义变量 ----------------
 var (
-	siteName = "HappeLife"
-	//blogPostsDir = "posts"
+	siteName     = "HappeLife"
+	blogPostsDir = "posts"
 	//blogPostsDir = "/media/jiftle/work/work/git/coding_net/grocery/dailylog"
-	blogPostsDir = "/usrlocal/git/coding_net/grocery/dailylog"
+//	blogPostsDir = "/usrlocal/git/coding_net/grocery/dailylog"
 )
 
 // 获取博客文章目录
